165eb8d2b1cd81ca537cede81d9bb81de981c748
Jayden Lee-Sin <Jayleesin@gmail.com>
2022-07-30 23:06:27 -0400

    Readability Improvements

--- Neww.ino
+++ Neww.ino
@@ -12,21 +12,25 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT);
   lastButtonState = digitalRead(BUTTON_PIN);
 }
+
 void loop() {
+  controlButtonPush();
+}
+
+void controlButtonPush() {
   if (millis() - lastTimeButtonStateChanged >= debounceDuration) {
   byte buttonState = digitalRead(BUTTON_PIN);
-  if (buttonState != lastButtonState) {
-  lastTimeButtonStateChanged = millis();
-  lastButtonState = buttonState;
- if (buttonState == LOW) {
-      if (ledState == HIGH) {
-        ledState = LOW;
-      }
-      else {
-        ledState = HIGH;
-      }
-      digitalWrite(LED_PIN, ledState);
+    if (buttonState != lastButtonState) {
+      lastTimeButtonStateChanged = millis();
+      lastButtonState = buttonState;
+        if (buttonState == LOW) {
+          if (ledState == HIGH) {
+            ledState = LOW;
+          }
+        else {
+          ledState = HIGH;
+        }
+        digitalWrite(LED_PIN, ledState);
     }
   }
-  }
-}
+ }
