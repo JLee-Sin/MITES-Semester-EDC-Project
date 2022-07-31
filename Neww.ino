4577cd76b9dce84b9b98bb2029878c1949f58892
Henok223 <110315544+Henok223@users.noreply.github.com>
2022-07-30 21:02:29 -0600

    Add files via upload

---
+++ Neww.ino
@@ -0,0 +1,32 @@
+#define LED_PIN 3
+#define BUTTON_PIN 2
+byte lastButtonState = LOW;
+byte ledState = LOW;
+
+unsigned long lastTimeButtonStateChanged = millis();
+unsigned long debounceDuration = 50;
+
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  lastButtonState = digitalRead(BUTTON_PIN);
+}
+void loop() {
+  if (millis() - lastTimeButtonStateChanged >= debounceDuration) {
+  byte buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState != lastButtonState) {
+  lastTimeButtonStateChanged = millis();
+  lastButtonState = buttonState;
+ if (buttonState == LOW) {
+      if (ledState == HIGH) {
+        ledState = LOW;
+      }
+      else {
+        ledState = HIGH;
+      }
+      digitalWrite(LED_PIN, ledState);
+    }
+  }
+  }
+}
