186e805f198511061dff6fd51ba449c1bda95725
Jayden Lee-Sin <Jayleesin@gmail.com>
2022-08-02 17:31:26 -0400

    Added verboseOut() and adjusted controlBurronPush()
    Added verbose output functionality to code; to enable verbose output please run a wire from the wiper of the potentiometer to the pin A0

--- Sketch.ino
+++ Sketch.ino
@@ -1,7 +1,10 @@
 #define LED_PIN 3
 #define BUTTON_PIN 2
+#define VERBOSE_PIN A0
 byte lastButtonState = LOW;
 byte ledState = LOW;
+float potVal = 0;
+float dutyCycle = 0;
 
 unsigned long lastTimeButtonStateChanged = millis();
 unsigned long debounceDuration = 50;
@@ -10,11 +13,14 @@ unsigned long debounceDuration = 50;
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
+  pinMode(VERBOSE_PIN, INPUT);
   lastButtonState = digitalRead(BUTTON_PIN);
 }
 
 void loop() {
   controlButtonPush();
+  verboseOut();
+  delay(50);
 }
 
 void controlButtonPush() {
@@ -25,14 +31,25 @@ void controlButtonPush() {
       lastButtonState = buttonState;
         if (buttonState == LOW) {
           if (ledState == HIGH) {
+            Serial.println("Circuit turned off");
             ledState = LOW;
           }
         else {
           ledState = HIGH;
+          Serial.println("Circuit turned on");
         }
         digitalWrite(LED_PIN, ledState);
     }
   }
  }
 }
+
+void verboseOut() {
+  if(potVal != analogRead(VERBOSE_PIN)) {
+    potVal = analogRead(VERBOSE_PIN);
+    float temp = (potVal/1023)*10;
+    Serial.println("Current speed is " + temp + "% ", 0);
+  }
+}
+  
 
